08d33a3e5f3b287c446685ad02575c9fe5d6fdbc
Louisa <lmarrone@tamu.edu>
2025-03-29 10:06:48 -0500

    barely while loop

--- sakura-coffee.py
+++ sakura-coffee.py
@@ -9,4 +9,13 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 320, 480
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Sakura Cafe")
+#image = pygame.image.load("Screenshot.png") #put the background here
 
+exit = False
+
+
+
+while not exit:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            exit = True
