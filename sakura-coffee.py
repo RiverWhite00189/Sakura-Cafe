ba4351fe4158a089cd3c5086a51f5fb1ff3e93e3
RiverWhite00189 <aparyani17@gmail.com>
2025-03-29 11:03:22 -0500

    titlescreen import

--- sakura-coffee.py
+++ sakura-coffee.py
@@ -3,22 +3,32 @@ import random
 import math
 
 
+#our functions
+import drawTitleScreen
+
+
 pygame.init()
+exit = False
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 320, 480
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Sakura Cafe")
 #image = pygame.image.load("Screenshot.png") #put the background here
 
-exit = False
+#colors 
+PINK = (251, 198, 207)
+screen.fill(PINK)
 
 personality1 = 0
 personality2 = 0
+
 
 while not exit:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT: 
             exit = True
+        else:
+            #run thorugh main program
+
 
         pygame.display.update()
